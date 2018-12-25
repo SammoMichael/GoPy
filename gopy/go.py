a2434de57fa66a850af02609d2b4e4e437f2894c
Samuel Michael <samadammichael@yahoo.com>
2018-12-24 20:07:04 -0500

    valid move and variable board size

--- gopy/go.py
+++ gopy/go.py
@@ -1,4 +1,6 @@
 import sys
+import pdb 
+import numpy as np
 
 def prRed(prt): print("\033[91m {}\033[00m" .format(prt))
 def prGreen(prt): print("\033[92m {}\033[00m" .format(prt))
@@ -9,26 +11,45 @@ def prCyan(prt): print("\033[96m {}\033[00m" .format(prt))
 def prLightGray(prt): print("\033[97m {}\033[00m" .format(prt))
 def prBlack(prt): print("\033[98m {}\033[00m" .format(prt))
 
-board_map = [
-    ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
-    ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
-    ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
-    ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
-    ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
-    ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
-    ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
-    ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
-    ['0', '0', '0', '0', '0', '0', '0', '0', '0']
-]
 
+
+board_size = int(input("board size?"))
+
+board_map = np.zeros( (board_size, board_size) )
+
+
+# board_map = [
+#     ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
+#     ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
+#     ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
+#     ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
+#     ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
+#     ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
+#     ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
+#     ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
+#     ['0', '0', '0', '0', '0', '0', '0', '0', '0']
+# ]
+
+# class Board 
+#     pass 
+    
 game = board_map
 players = ['player1', 'player2']
 turns = [0, 1]
 current_turn = 1
 
+def occupied_space(x, y):
+    if game[x][y] != 0:
+        return True 
+    return False 
+
 def current_player_mark():
     return {'player1': '1', 'player2': '2'}.get(current_player())
 
+def current_player_color():
+    if current_player() == 1:
+        return 'white'
+    return 'black'
 
 def current_player():
     global current_turn 
@@ -37,27 +58,33 @@ def current_player():
     return players[(current_turn) % 2]
 
 
-def check_valid_move(x, y):
-    return valid_move(x, y)
+# def check_valid_move(x, y):
+#     return valid_move(x, y)
 
 
-def valid_move(x, y):
-    global board_map
-    if board_map[x][y] == 1 or board_map[x][y] == 2:
-        return False
-    return True
+# def []=:
+#     pass
+
+# def check_liberties: 
+#     for x in board_map:
+#         pass 
 
 
-def update_board(board_map, player = 0, row = 0, col = 0):
+# def invalid_move(x, y):
+#     return board_map[x][y] == 1 or board_map[x][y] == 2
+
+
+def update_board(board_map = board_map, player = 0, row = 0, col = 0):
     try: 
-        # print(chr(27) + "[2J")
-        prGreen('   0  1  2  3  4  5  6  7  8')
-        board_map[row][col] = player
-        for count, row in enumerate(board_map):
-            prRed(row)
-            prGreen(count)
-            print(board_map)
-        return board_map or ''
+        if board_map is not None:
+            print(chr(27) + "[2J")
+            prGreen('   0  1  2  3  4  5  6  7  8')
+            board_map[row][col] = player
+            for count, row in enumerate(board_map):
+                prPurple(row)
+                prGreen(count)
+                # print(board_map)
+            return board_map
     except IndexError as e: print(e)
     except: print('sorry, something went awry')
 
@@ -67,24 +94,28 @@ def get_move():
         move = input("move")
         move_x = (int(move[0]))
         move_y = (int(move[1]))
-        valid = (valid_move(move_x, move_y))
-        if valid == False:
-            raise ValueError() 
+        if occupied_space(move_x, move_y):
+            raise Exception
+        # pdb.set_trace()
+        # invalid = (invalid_move(move_x, move_y))
+        # print(invalid)
+        # if invalid == True:
+        #     raise ValueError() 
         print([move_x, move_y])           
-        return [move_x, move_y] or ''
+        return [move_x, move_y]
     except Exception as e: print("invalid move", e)
     get_move()
 
 
 def game_loop(board_map):
     try:
-        game_over = False
-        while (game_over != True):
-            new_move = get_move()
-            game = update_board(board_map, current_player_mark(), new_move[0], new_move[1])
-        print(game)
-        return game or ''
+        if board_map is not None:
+            game_over = False
+            while (game_over != True):
+                new_move = get_move()
+                if new_move is not None:
+                    game = update_board(board_map, current_player_mark(), new_move[0], new_move[1])
+            return game
     except TypeError as e: print(e)
-    raise
-
+    game = update_board(board_map, current_player_mark(),new_move[0], new_move[1])
 game_loop(game)
