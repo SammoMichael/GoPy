94293e7cf502f37c55238f7d1d737a480802f5a7
Samuel Michael <samadammichael@yahoo.com>
2018-12-24 23:07:12 -0500

    fix index error

--- gopy/go.py
+++ gopy/go.py
@@ -11,32 +11,55 @@ def prCyan(prt): print("\033[96m {}\033[00m" .format(prt))
 def prLightGray(prt): print("\033[97m {}\033[00m" .format(prt))
 def prBlack(prt): print("\033[98m {}\033[00m" .format(prt))
 
-
-
 board_size = int(input("board size?"))
-
 board_map = np.zeros( (board_size, board_size) )
-
-
-# board_map = [
-#     ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
-#     ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
-#     ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
-#     ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
-#     ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
-#     ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
-#     ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
-#     ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
-#     ['0', '0', '0', '0', '0', '0', '0', '0', '0']
-# ]
-
-# class Board 
-#     pass 
-    
 game = board_map
 players = ['player1', 'player2']
 turns = [0, 1]
 current_turn = 1
+
+def on_edge(x, y):
+    if x == 0 and (y != 0 or y != board_size - 1):
+        return 'w'
+    elif x == board_size - 1 and (y != board_size - 1 or y != 0):
+        return 'e'
+    elif y == 0 and (x != 0 or x != board_size - 1):
+        return 'n'
+    elif y == board_size - 1 and (x != board_size - 1 or x != 0):
+        return 's'
+    return False
+
+def on_corner(x, y):
+    if (x == 0 and y == 0):
+        return 'nw' 
+    elif (x == board_size - 1 and y == board_size - 1 ):
+        return 'se'  
+    elif (x == board_size - 1 and y == 0): 
+        return 'ne'
+    elif (y == board_size - 1 and x == 0):
+        return 'sw'
+    return False
+
+def on_board(x, y):
+    if (x >= 0 and x <= board_size - 1) and (y >= 0 and y <= board_size - 1):
+        return True 
+    return False 
+
+def check_liberties(x, y):
+    global game
+    # if on_edge(x, y):
+    #     print(on_edge(x, y))
+    # if on_corner(x, y):
+    #     print(on_corner(x, y))
+    # if game[x+1][y] == 2 and game[x][y+1] and game[x][y-1] == 2 and game[x-1][y] == 2:
+    # game[x][y] = 0
+    if game[x][y] == 1:
+        if game[x+1][y] == 2 and game[x][y+1] and game[x][y-1] == 2 and game[x-1][y] == 2:
+            game[x][y] = 0
+    if game[x][y] == 2:
+        if game[x+1][y] == 2 and game[x][y+1] and game[x][y-1] == 2 and game[x-1][y] == 1:
+            game[x][y] = 0
+    
 
 def occupied_space(x, y):
     if game[x][y] != 0:
@@ -57,58 +80,102 @@ def current_player():
     print(players[(current_turn) % 2])
     return players[(current_turn) % 2]
 
+white_groups = []
+black_groups = []
 
-# def check_valid_move(x, y):
-#     return valid_move(x, y)
+# def check_adjacency(x,y):
+#     if [x, y] in white_groups or [x, y] in black_groups:
+#         return 
+#     # print(black_groups)
+#     # print(white_groups)
+#     if game[x][y] == 2:
+#             # white_groups.append()
+#             if game[x+1][y] == 2:
+#                 if [x+1][y] not in white_groups:
+#                     white_groups.append([x+1, y])
+#                 elif [x][y] not in white_groups:
+#                     white_groups.append([x, y])
+#                 # check_adjacency(x+1, y)
+#                     # black_groups.append([x, y], [x+1, y])
+#             elif game[x-1][y] == 2:
+#                 if [x-1][y] not in white_groups:
+#                     white_groups.append([x-1, y])
+#                 elif [x][y] not in white_groups:
+#                     white_groups.append([x, y])
 
+#                 # black_groups.append([x, y], [x-1, y])
 
-# def []=:
-#     pass
+#             elif game[x][y-1] == 2:
+#                 if [x][y-1] not in white_groups:
+#                     white_groups.append([x, y-1])
+#                 elif [x][y] not in white_groups:
+#                     white_groups.append([x, y])
 
-# def check_liberties: 
-#     for x in board_map:
-#         pass 
+#                 # black_groups.append([x, y], [x, y-1])
 
+#             elif game[x][y+1] == 2:
+#                 if [x][y+1] not in white_groups:
+#                     white_groups.append([x, y+1])
 
-# def invalid_move(x, y):
-#     return board_map[x][y] == 1 or board_map[x][y] == 2
+#                 # black_groups.append([x, y], [x, y+1])
+#     if game[x][y] == 1:
+#             if game[x+1][y] == 1:
+#                 print('adjacent')
 
+#                 # black_groups.append([x, y], [x+1, y])
+#             elif game[x-1][y] == 1:
+#                 print('adjacent')
+
+#                 # black_groups.append([x, y], [x-1, y])
+
+#             elif game[x][y-1] == 1:
+#                 print('adjacent')
+
+#                 # black_groups.append([x, y], [x, y-1])
+
+#             elif game[x][y+1] == 1:
+#                 print('adjacent')
+
+#                 # black_groups.append([x, y], [x, y+1])
+         
 
 def update_board(board_map = board_map, player = 0, row = 0, col = 0):
     try: 
         if board_map is not None:
-            print(chr(27) + "[2J")
+            # print(chr(27) + "[2J")
             prGreen('   0  1  2  3  4  5  6  7  8')
             board_map[row][col] = player
             for count, row in enumerate(board_map):
+                for count2, stone in enumerate(row): 
+                    # pdb.set_trace()
+                    check_liberties(count, count2)
+                    # check_adjacency(count2, count)
                 prPurple(row)
                 prGreen(count)
                 # print(board_map)
+                # pdb.set_trace()
             return board_map
     except IndexError as e: print(e)
     except: print('sorry, something went awry')
 
-
 def get_move():
     try:
         move = input("move")
-        move_x = (int(move[0]))
-        move_y = (int(move[1]))
+        move_y = (int(move[0]))
+        move_x = (int(move[1]))
         if occupied_space(move_x, move_y):
             raise Exception
+        if on_board(move_x, move_y) == False:
+            raise Exception
         # pdb.set_trace()
-        # invalid = (invalid_move(move_x, move_y))
-        # print(invalid)
-        # if invalid == True:
-        #     raise ValueError() 
         print([move_x, move_y])           
         return [move_x, move_y]
     except Exception as e: print("invalid move", e)
-    get_move()
-
+    # get_move()
 
 def game_loop(board_map):
     try:
+        update_board()
         if board_map is not None:
             game_over = False
             while (game_over != True):
